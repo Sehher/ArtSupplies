b0da13ba18da402c1ab3afac93e7d4339a491c41
SeasonsFate <alexbrew6@gmail.com>
2019-09-18 08:43:29 +0100

    added product service

---
+++ src/main/java/com/qa/controllers/ProductsController.java
@@ -0,0 +1,32 @@
+package com.qa.controllers;
+
+import com.qa.models.Products;
+import com.qa.repository.ProductRepository;
+import com.qa.service.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+public class ProductsController {
+
+    @Autowired
+    private ProductsService productsService;
+
+    @RequestMapping(value = "products", method = RequestMethod.GET)
+    public List<Products> listAllProducts(){
+        return productsService.listAllProducts();
+    }
+
+    @RequestMapping(value = "products",method = RequestMethod.POST)
+    public Products addNote(@RequestBody Products products){
+        return productsService.addProduct(products);
+    }
+
+    @RequestMapping(value = "products/{id}", method = RequestMethod.DELETE)
+    public Products deleteProductFromList(@PathVariable Long id){
+       return productsService.deleteProduct(id);
+    }
+}
---
+++ src/main/java/com/qa/service/ProductsService.java
@@ -0,0 +1,29 @@
+package com.qa.service;
+
+import com.qa.models.Products;
+import com.qa.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductsService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Products> listAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public Products addProduct( Products products){
+        return productRepository.saveAndFlush(products);
+    }
+
+    public Products deleteProduct( Long id){
+        Products existing = productRepository.findOne(id);
+        productRepository.delete(existing);
+        return existing;
+    }
+}
